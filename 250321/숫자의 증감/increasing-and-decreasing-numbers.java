a88f4b4a07912e266fd1fde355f74b1bcb3c0067
Taetaeeee <89624929+Taetaeeee@users.noreply.github.com>
2025-03-21 23:51:21 +0900

    Title: 숫자의 증감, Time: 3033ms, Memory: 35MB, Status: Time Limit Exceed - CodeTree

---
+++ 250321/숫자의 증감/increasing-and-decreasing-numbers.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String c = sc.next();
+        char word = c.charAt(0);
+        int n = sc.nextInt();
+
+        if(word == 'A'){
+            for(int i=1;i<=n;i++){
+                System.out.print(i + " ");
+            }
+        }
+        else if(word == 'D'){
+            for(int i=n;n>=1;i--){
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
