a79f8a2ff1d295ef05e03c135a5f96ffb2cd61c9
Taetaeeee <89624929+Taetaeeee@users.noreply.github.com>
2025-03-28 15:25:13 +0900

    Title: 1까지 나누기, Time: 108ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250328/1까지 나누기/divide-by-1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int n1 = n;
+        int cnt=0;
+
+        for(int i=1;i<=n1;i++){
+            cnt++;
+            if(n/i<=1){
+                System.out.print(cnt);
+                break;
+            }
+            n = n/i;
+        }
+    }
+}
