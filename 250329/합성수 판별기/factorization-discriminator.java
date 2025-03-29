2ad2687f53980d9b3ccd2414699591848a142cb4
Taetaeeee <89624929+Taetaeeee@users.noreply.github.com>
2025-03-29 21:40:51 +0900

    Title: 합성수 판별기, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250329/합성수 판별기/factorization-discriminator.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean judge = false;
+
+        for(int i=2;i<=n-1;i++){
+            if(n%i==0){
+                judge = true;
+            }
+        }
+
+        if(judge){
+            System.out.print("C");
+        }
+        else{
+            System.out.print("N");
+        }
+        
+    }
+}
