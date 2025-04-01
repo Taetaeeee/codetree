922e5989e77d7cf21eb29a79ee63d0471d3b7e5c
Taetaeeee <89624929+Taetaeeee@users.noreply.github.com>
2025-04-01 14:42:07 +0900

    Title: 특정 위치의 문자 2, Time: 120ms, Memory: 10MB, Status: Passed - Codetree

--- 250401/특정 위치의 문자 2/char-in-specific-location-2.java
+++ 250401/특정 위치의 문자 2/char-in-specific-location-2.java
@@ -4,11 +4,10 @@ public class Main {
     public static void main(String[] args) {
         // Please write your code here.
         Scanner sc = new Scanner(System.in);
-        String[] arr = new String[10];
+        char[] arr = new char[10];
 
         for(int i=0;i<10;i++){
-            String s = sc.next();
-            arr[i]= s;
+            arr[i]= sc.next().charAt(0);
         }
         System.out.print(arr[1] + " " + arr[4] + " "+ arr[7]);
     }
