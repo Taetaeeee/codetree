f5200baaa647ec44781320cb8e14b676de3e4593
Taetaeeee <89624929+Taetaeeee@users.noreply.github.com>
2025-04-01 13:23:46 +0900

    Title: 특정 위치의 수, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250401/특정 위치의 수/numbers-in-specific-location.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+
+        for(int i=0;i<10;i++){
+            int n = sc.nextInt();
+            arr[i] = n;
+        }
+
+        System.out.print(arr[2] + arr[4] + arr[9]);
+    }
+}
