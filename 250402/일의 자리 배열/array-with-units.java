a763807ab9396a715e11fefec6ed812699cfbf00
Taetaeeee <89624929+Taetaeeee@users.noreply.github.com>
2025-04-02 13:17:12 +0900

    Title: 일의 자리 배열, Time: 112ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250402/일의 자리 배열/array-with-units.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int [] arr = new int[10];
+
+        arr[0] = sc.nextInt();
+        arr[1] = sc.nextInt();
+
+        for(int i=3;i<10;i++){
+            int n = arr[i-1] + arr[i-2];
+            if(n>=10){
+                arr[i] = n%10;
+            }
+            else
+            {
+                arr[i] = n;
+            }
+            
+        }
+        for(int i=0;i<10;i++){
+            System.out.print(arr[i] + " ");
+        }
+        
+    }
+}
