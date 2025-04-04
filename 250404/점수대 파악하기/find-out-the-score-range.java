c77f3389c8ad38e5d84a3b175195a29da862b681
Taetaeeee <89624929+Taetaeeee@users.noreply.github.com>
2025-04-04 16:16:13 +0900

    Title: 점수대 파악하기, Time: 110ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250404/점수대 파악하기/find-out-the-score-range.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[11];
+
+        while(true)
+        {
+            int n = sc.nextInt();
+            if(n==0){
+                break;
+            }
+
+            arr[n/10]++;
+        }
+        for(int i=10;i>0;i--){
+            System.out.println(i*10 + " - " + arr[i]);
+        }
+    }
+}
