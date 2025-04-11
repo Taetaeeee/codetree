0aecc1948f93b15ab4dc4caea2bc05e2791fbd11
Taetaeeee <89624929+Taetaeeee@users.noreply.github.com>
2025-04-11 09:34:22 +0900

    Title: 999 또는 -999, Time: 122ms, Memory: 10MB, Status: Runtime Error - Codetree

--- 250411/999 또는 -999/999-or-999.java
+++ 250411/999 또는 -999/999-or-999.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // Please write your code here.
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt()
+        int n = sc.nextInt();
         int min = n;
         int max = n;
 
