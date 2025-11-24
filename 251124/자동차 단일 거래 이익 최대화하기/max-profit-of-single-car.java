a73639cf92c5b1079753b55819754021ab7864dd
Taetaeeee <89624929+Taetaeeee@users.noreply.github.com>
2025-11-24 17:25:19 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 197ms, Memory: 12MB, Status: Passed - Codetree

--- 251124/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
+++ 251124/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
@@ -6,27 +6,19 @@ public class Main {
 
         int N = sc.nextInt();
         int [] arr = new int[N];
-        int buycar = -1;
 
         for(int i=0;i<N;i++){
             arr[i] = sc.nextInt();
         }
 
         int car = 0;
-        for(int i=1;i<N;i++){
-            if(arr[car]>arr[i])
-                car = i;
-        }
-        if(car==N-1){
-            System.out.println("0");
-        }
-        else{
-            int sellcar = car;
-            for(int i=car;i<N;i++){
-                if(arr[sellcar]<arr[i])
-                    sellcar = i;
+        for(int i=0;i<N;i++){
+            for(int j=i+1;j<N;j++){
+                int profit = arr[j] - arr[i];
+                if(profit>car)
+                    car = profit;
             }
-            System.out.println(arr[sellcar]-arr[car]);
         }
+        System.out.println(car);
     }
 }
