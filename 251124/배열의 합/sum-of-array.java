0a8c11e5f601f2b1b0da2a4831368fbb3d79c7b8
Taetaeeee <89624929+Taetaeeee@users.noreply.github.com>
2025-11-24 19:44:06 +0900

    Title: 배열의 합, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

--- 251124/배열의 합/sum-of-array.java
+++ 251124/배열의 합/sum-of-array.java
@@ -3,12 +3,18 @@ public class Main {
     public static void main(String[] args) {
         // Please write your code here.
         Scanner sc = new Scanner(System.in);
+        int [][] arr = new int[4][4];
+
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
 
         for(int i=0;i<4;i++){
             int sum = 0;
             for(int j=0;j<4;j++){
-                int n = sc.nextInt();
-                sum+=n;
+                sum+=arr[i][j];
             }
             System.out.println(sum);
         }
