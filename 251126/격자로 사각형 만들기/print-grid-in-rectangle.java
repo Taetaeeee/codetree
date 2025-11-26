cd471d1242a2dc6b5978370e596a0dc0157c0a96
Taetaeeee <89624929+Taetaeeee@users.noreply.github.com>
2025-11-26 21:28:38 +0900

    Title: 격자로 사각형 만들기, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251126/격자로 사각형 만들기/print-grid-in-rectangle.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int [][] arr2d = new int[N][N];
+
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                arr2d[0][i] = 1;
+                arr2d[j][0] = 1;
+            }
+        }
+
+        for(int i=1;i<N;i++){
+            for(int j=1;j<N;j++){
+                arr2d[i][j] = arr2d[i-1][j-1] + arr2d[i-1][j] + arr2d[i][j-1];
+            }
+        }
+
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                System.out.print(arr2d[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
