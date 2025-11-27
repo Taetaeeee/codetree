5823bbc5c5958aff276b9818dba66f72422b0b5c
Taetaeeee <89624929+Taetaeeee@users.noreply.github.com>
2025-11-27 14:44:18 +0900

    Title: 문자 개수 세기, Time: 109ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251127/문자 개수 세기/count-char.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String S = sc.nextLine();
+        char c = sc.next().charAt(0);
+
+        int cnt = 0;
+        for(int i=0;i<S.length();i++){
+            if(S.charAt(i) == c)
+                cnt++;
+        }
+
+        System.out.print(cnt);
+    }
+}
