3f692c67a5c4501bff5fa297a090eb624f43a538
Taetaeeee <89624929+Taetaeeee@users.noreply.github.com>
2025-11-27 15:01:43 +0900

    Title: 공백을 기준으로 출력, Time: 143ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251127/공백을 기준으로 출력/output-based-on-space.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String S_1 = sc.nextLine().replace(" ","");
+        String S_2 = sc.nextLine().replace(" ","");
+
+        System.out.print(S_1 + S_2);
+
+    }
+}
