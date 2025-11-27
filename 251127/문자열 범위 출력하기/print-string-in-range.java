25980caa2c06a9531553cadf47c04daf60ef728b
Taetaeeee <89624929+Taetaeeee@users.noreply.github.com>
2025-11-27 14:31:22 +0900

    Title: 문자열 범위 출력하기, Time: 118ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251127/문자열 범위 출력하기/print-string-in-range.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        String S = sc.nextLine();
+
+        for(int i=2;i<10;i++){
+            System.out.print(S.charAt(i));
+        }
+    }
+}
