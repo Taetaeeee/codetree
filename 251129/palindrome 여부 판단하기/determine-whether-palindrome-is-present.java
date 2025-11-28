2f053dcfbda793fb59892303bc44999fdc9ae46c
Taetaeeee <89624929+Taetaeeee@users.noreply.github.com>
2025-11-29 01:21:20 +0900

    Title: palindrome 여부 판단하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251129/palindrome 여부 판단하기/determine-whether-palindrome-is-present.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static boolean issame(String s){
+        int len = s.length();
+
+        boolean istrue = true;
+        for(int i=0;i<len;i++){
+            if(s.charAt(i)==s.charAt(len-i))
+            else
+                istrue=false;
+            
+        }
+
+        return istrue;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        // Please write your code here.
+        if(issame(input))
+            System.out.print("Yes");
+        else
+            System.out.print("No");
+    }
+}
