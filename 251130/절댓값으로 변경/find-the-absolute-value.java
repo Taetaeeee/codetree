c239060c52e9cf85f9da0a41d85a19da7db8d3a0
Taetaeeee <89624929+Taetaeeee@users.noreply.github.com>
2025-11-30 19:31:52 +0900

    Title: 절댓값으로 변경, Time: 115ms, Memory: 10MB, Status: Passed - Codetree

--- 251130/절댓값으로 변경/find-the-absolute-value.java
+++ 251130/절댓값으로 변경/find-the-absolute-value.java
@@ -1,24 +1,27 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int perfectNum(int n){
-        if(n<0)
-        {
-            n*=(-1);
+    public static final int MAX_N = 50;
+    public static int n;
+    public static int[] arr = new int[MAX_N];
+    public static void perfectNum(int[] arr){
+        for(int i=0;i<n;i++){
+            if(arr[i]<0){
+                arr[i] = -arr[i];
+            }
         }
-
-        return n;
+        return;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
+        n = sc.nextInt();
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
         // Please write your code here.
+        perfectNum(arr);
+
         for(int i=0;i<n;i++){
-            arr[i] = perfectNum(arr[i]);
             System.out.print(arr[i] + " ");
         }
     }
