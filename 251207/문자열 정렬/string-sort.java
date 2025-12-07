0731066c0d08ecdf7e9eb32b585bbfa4b061b097
Taetaeeee <89624929+Taetaeeee@users.noreply.github.com>
2025-12-07 12:29:19 +0900

    Title: 문자열 정렬, Time: 116ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251207/문자열 정렬/string-sort.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        // Please write your code here.
+        char[] chars = s.toCharArray();
+
+        Arrays.sort(chars);
+
+        String sorts = new String(chars);
+        System.out.print(sorts);
+    }
+}
