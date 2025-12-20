a92d2002f2f91498790c7bfcf00aa75d5fca363e
Taetaeeee <89624929+Taetaeeee@users.noreply.github.com>
2025-12-21 01:35:46 +0900

    Title: 비오는 날, Time: 147ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251221/비오는 날/rainy-day.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+class Predict{
+    String date;
+    String day;
+    String weather;
+
+    public Predict(String date, String day, String weather){
+        this.date = date;
+        this.day = day;
+        this.weather = weather;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Predict[] predict1 = new Predict[n];
+        for (int i = 0; i < n; i++) {
+            String date = sc.next();
+            String day = sc.next();
+            String weather = sc.next();
+            // Please write your code here.
+
+            predict1[i] = new Predict(date, day, weather);
+        }
+
+        int rainidx = 0;
+        for(int i=0;i<n;i++){
+            if(predict1[i].weather.equals("Rain")){
+                rainidx = i;
+                break;
+            }
+        }
+
+        System.out.print(predict1[rainidx].date + " ");
+        System.out.print(predict1[rainidx].day + " ");
+        System.out.print(predict1[rainidx].weather);
+    }
+}
