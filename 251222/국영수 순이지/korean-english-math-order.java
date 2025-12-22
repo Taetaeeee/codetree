a8fbf65e19e73ee0a9995e4bfefcc6df9661885e
Taetaeeee <89624929+Taetaeeee@users.noreply.github.com>
2025-12-22 22:57:32 +0900

    Title: 국영수 순이지, Time: 132ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251222/국영수 순이지/korean-english-math-order.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+class Student implements Comparable<Student>{
+    String name;
+    int korean;
+    int english;
+    int math;
+
+    public Student(String name, int korean, int english, int math){
+        this.name = name;
+        this.korean = korean;
+        this.english = english;
+        this.math = math;
+    }
+
+    @Override
+    public int compareTo(Student student){
+        if(this.korean == student.korean){
+            if(this.english == student.english){
+                return student.math - this.math;
+            }
+            return student.english - this.english;
+        }
+        return student.korean - this.korean;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] names = new String[n];
+        int[] korean = new int[n];
+        int[] english = new int[n];
+        int[] math = new int[n];
+        Student[] students = new Student[n];
+        for (int i = 0; i < n; i++) {
+            names[i] = sc.next();
+            korean[i] = sc.nextInt();
+            english[i] = sc.nextInt();
+            math[i] = sc.nextInt();
+
+            students[i] = new Student(names[i], korean[i], english[i], math[i]);
+        }
+        // Please write your code here.
+        Arrays.sort(students);
+
+        for(int i=0;i<n;i++){
+            System.out.print(students[i].name + " ");
+            System.out.print(students[i].korean + " ");
+            System.out.print(students[i].english + " ");
+            System.out.println(students[i].math);
+        }
+    }
+}
