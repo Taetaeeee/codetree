5f3cb1ee03f21c314e404ea5318b9548ad680d73
Taetaeeee <89624929+Taetaeeee@users.noreply.github.com>
2026-05-25 01:14:16 +0900

    [Passed] 여러가지 진수변환 (135ms, 13MB)

---
+++ 260525/여러가지 진수변환/various-numeral-system-transformations.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int b = sc.nextInt();
+        int [] digits = new int[20];
+
+        int cnt = 0;
+
+        while(true){
+            if(b==4){
+                if(n<4){
+                    digits[cnt++] = n;
+                    break;
+                }
+                digits[cnt++] = n%4;
+                n/=4;
+            }
+            else if(b==8){
+                if(n<8){
+                    digits[cnt++] = n;
+                    break;
+                }
+                digits[cnt++] = n%8;
+                n/=8;
+            }
+        }
+
+        for(int i=cnt-1;i>=0;i--){
+            System.out.print(digits[i]);
+        }
+
+    }
+}
