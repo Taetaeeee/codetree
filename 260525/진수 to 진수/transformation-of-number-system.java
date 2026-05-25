dacfdd51fde4fb059e6ac5bdf0bfde830081eb20
Taetaeeee <89624929+Taetaeeee@users.noreply.github.com>
2026-05-25 13:59:24 +0900

    [Passed] 진수 to 진수 (151ms, 10MB)

---
+++ 260525/진수 to 진수/transformation-of-number-system.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        String N = sc.next();
+        int num = 0;
+
+        //num에 N값을 10진수로 변환한 값을 입력.
+        for(int i=0;i<N.length();i++){
+            num = num*A + (N.charAt(i) - '0');
+        }
+
+        int[] digits = new int[20];
+        int cnt = 0;
+
+        while(true){
+            if(num<B){
+                digits[cnt++] = num;
+                break;
+            }
+
+            digits[cnt++] = num%B;
+            num/=B;
+        }
+        for(int i=cnt-1;i>=0;i--){
+            System.out.print(digits[i]);
+        }
+    }
+}
